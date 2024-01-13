7337f3b789f72ddc7fc1bc24b9a913628f5af542
patrickmuchiri <pmuchirig@gmail.com>
2024-01-13 01:22:57 -0800

    implemented the show command

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 """ The console command interpreter """
 
 import cmd
+import models
 from models.base_model import BaseModel
 
 
@@ -39,7 +40,25 @@ class HBNBCommand(cmd.Cmd):
         if isinstance(new_instance, BaseModel):
             new_instance.save()
             print(new_instance.id)
+
         return
+
+    def do_show(self, args):
+        """
+        Prints the string representation of an instance
+        based on the class name and id.
+        Args:
+            args(string): A string of command line inputs
+        """
+        command = args.split()
+        if not self.verify_class_name(command):
+            return
+        if not self.verify_id(command):
+            return
+        key = '{}.{}'.format(command[0], command[1])
+        objs = models.storage.all()
+
+        print(objs[key])
 
     @classmethod
     def verify_class_name(cls, command):
@@ -50,6 +69,21 @@ class HBNBCommand(cmd.Cmd):
         elif command[0] not in HBNBCommand.my_class_list:
             print("** class doesn't exist **")
             return False
+        return True
+
+    @staticmethod
+    def verify_id(command):
+        """verify if the id is in list of objects"""
+        if len(command) < 2:
+            print("** instance id missing **")
+            return False
+        objs = models.storage.all()
+        key = '{}.{}'.format(command[0], command[1])
+
+        if key not in objs.keys():
+            print("** no instance found **")
+            return False
+
         return True
 
 
